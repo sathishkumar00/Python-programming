f825d79096b0654affa04b325c45638672edb18e
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-06-04 10:26:16 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,7 @@
+p=input()
+if(p=='a' or p=='e' or p=='i' or p=='o' or p=='u' or p=='A' or p=='E' or p=='I' or p=='O' or p=='U'):
+          print("Vowel")
+elif(p=='$'):
+          print("Invalid")
+else:
+          print("Consonant")
