3589c8594b4e96ff3a2cdda80945150c7b2d9097
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-06-04 11:39:17 +0530

    Create largestno.py

---
+++ largestno.py
@@ -0,0 +1,8 @@
+p=input()
+p=p.split(" ")
+if(int(p[0])>int(p[1]) and int(p[0])>int(p[2])):
+     print(int(p[0]0)
+elif(int(p[1])>int(p[0]) and int(p[1])>int(p[2])):
+     print(int(p[1]))
+else:
+     print(int(p[2]))
