e7fe52ecc2914991784db2f875274eab20e8c2ea
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-06-04 11:42:23 +0530

    Update largestno.py

--- largestno.py
+++ largestno.py
@@ -1,7 +1,7 @@
 p=input()
 p=p.split(" ")
 if(int(p[0])>int(p[1]) and int(p[0])>int(p[2])):
-     print(int(p[0]0)
+     print(int(p[0]))
 elif(int(p[1])>int(p[0]) and int(p[1])>int(p[2])):
      print(int(p[1]))
 else:
