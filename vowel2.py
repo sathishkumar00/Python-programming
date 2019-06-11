e430272b45bed5465a1d455fd15a78e65d2e1c9e
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-06-11 11:09:37 +0530

    Create vowel2.py

---
+++ vowel2.py
@@ -0,0 +1,11 @@
+c=input()
+d=0
+for i in c:
+     if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+         d=1
+if(d==1):
+       print("yes")
+else:
+       print("no")
+         
+          
