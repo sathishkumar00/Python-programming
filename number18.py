a26529101ea36f16fc9084c191fc4fba6b1a8d98
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-06-11 14:40:02 +0530

    Update number18.py

--- number18.py
+++ number18.py
@@ -1,6 +1,6 @@
-def print_factors(p):
-     for i in range(1,p+1):
-        if(p%i==0):
-             print(i)
-q=int(input())
-print(print_factors(q))
+f=int(input())
+l=[]
+for i in range(1,f+1):
+     if(f%i==0):
+          l.append(i)
+print(*l,sep=" ")
