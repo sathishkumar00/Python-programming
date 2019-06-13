395e70634636d31ed5c9fff65bebfae4e8c60e74
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-06-13 13:13:33 +0530

    Create string17.py

---
+++ string17.py
@@ -0,0 +1,11 @@
+f=input()
+l=[]
+g=[]
+for i in range(0,len(f)):
+       if(i%2==0):
+           l.append(f[i])
+       else:
+           g.append(f[i])
+for i in range(0,len(f))
+   print(g[i],end="")
+   print(l[i],end="")
