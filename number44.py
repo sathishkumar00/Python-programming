ae35507d4b0b66e417a8ee80a4383165a3df06c7
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-06-17 15:58:05 +0530

    Update number44.py

--- number44.py
+++ number44.py
@@ -1,11 +1,11 @@
 def function(f):
         if(f==0):
-            return false
+            return False
         while(f!=1):
              if(f%2==0):
-                  return false
+                  return False
              f=f//2
-        return true
+        return True
 f=int(input())
 if(function(f)):
        print("yes")
