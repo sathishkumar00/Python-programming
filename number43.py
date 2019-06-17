811e9e5fadcc3f0ea7a4084dec963f730951f2d9
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-06-17 15:18:53 +0530

    Update number43.py

--- number43.py
+++ number43.py
@@ -2,6 +2,6 @@ f=int(input())
 l=[]
 for i in range(1,f+1):
        if(f%i==0):
-             if(i%2==0):
-                  l.append(i)
+              if(i%2==0):
+                     l.append(i)
 print(*l,sep=" ")
