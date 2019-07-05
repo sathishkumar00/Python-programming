6d71721ea511fe9ae53339bea78995b48f921c58
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-07-05 20:58:14 +0530

    Update number73.py

--- number73.py
+++ number73.py
@@ -6,7 +6,7 @@ m=[]
 for i in range(0,len(k)):
      l=i+1
      for j in range(l,len(k)):
-         if(k[i]==k[j]):
+         if(k[i]==k[j] and k[i] not in m):
               m.append(k[i])
 m=sorted(m)
 if(m):
