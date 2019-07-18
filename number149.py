e5d240e44663b27087059029205545267d2dd2d4
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-07-18 20:43:49 +0530

    Create number149.py

---
+++ number149.py
@@ -0,0 +1,11 @@
+l=int(input())
+m=list(map(int,input().split(" ")))
+n=0
+for i in range(1,100000):
+            n=0
+            for j in range(0,len(l)):
+                if(i%m[j]==0):
+                      n=n+1
+            if(n==l):
+                  print(i)
+                  break
