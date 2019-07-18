0177244928a5ed5ae7c001935f47872f52b529cc
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-07-18 20:47:46 +0530

    Update number149.py

--- number149.py
+++ number149.py
@@ -3,7 +3,7 @@ m=list(map(int,input().split(" ")))
 n=0
 for i in range(1,100000):
             n=0
-            for j in range(0,len(l)):
+            for j in range(0,l):
                 if(i%m[j]==0):
                       n=n+1
             if(n==l):
