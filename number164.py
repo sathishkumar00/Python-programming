185f93daf14407cef4fe56d0adadcac78d71806c
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-07-19 18:45:04 +0530

    Update number164.py

--- number164.py
+++ number164.py
@@ -1,6 +1,7 @@
-d=int(input())
-e=list(map(int,input().split(" ")))
-e=sorted(e)
-f=e[ ::-1]
-for i in f:
-     print(i)
+c=int(input())
+d=list(map(int,input().split(" ")))
+d=[bin(i) for i in d]
+d.sort(reverse=True)
+d=[int(c,2) for c in d]
+for i in range(0,c):
+     print(d[i])
