5d6f0a08edd7ad10ea5f9aa1e722b6bdc74e9c64
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-07-23 18:44:07 +0530

    Update number176.py

--- number176.py
+++ number176.py
@@ -1,13 +1,11 @@
 d,e=list(map(int,input().split(" ")))
 f=list(map(int,input().split(" ")))
 g=[]
-for i in f:
-      if(i==e):
-           print(i)
-           break
-      else:
-           for i in f:
-               if(i<e):
-                   g.append(i)
-               g=sorted(g)
-               print(g[-1])
+if e in f:
+      print(e)
+else:
+   for i in f:
+           if(i<e):
+              g.append(i)
+           g=sorted(g)
+           print(g[-1])
