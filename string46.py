ff8e7b560f5945b50c6b507564342f5730c4a8db
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-07-23 15:49:17 +0530

    Update string46.py

--- string46.py
+++ string46.py
@@ -2,9 +2,10 @@ d=input()
 c=0
 for i in d:
     if(i!='a' and i!='b'):
-        print("no")
-        break
+        c=0
     else:
         c=c+1
 if(c>0):
      print("yes")
+else:
+     print("no")
