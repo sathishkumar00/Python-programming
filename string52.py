a79d004a08573eef0fe496a33d4cc4eb3495a548
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-07-26 19:05:30 +0530

    Create string52.py

---
+++ string52.py
@@ -0,0 +1,21 @@
+d=input()
+e=input()
+f=[]
+if(d.isdigit()):
+      g=list(d)
+      h=list(e)
+else:
+      g=d.split(" ")
+      h=e.split(" ")
+if(len(g)==len(h)):
+    for i in range(len(g)-1):
+       if(g[i]!=h[i]):
+              f.append(g[i])
+    for i in range(len(g)-1):
+           if(h[i]!=g[i]):
+              f.append(h[i])
+else:
+  for i in g:
+     if i not in h:
+           f.append(i)
+print(*f,sep=" ")
