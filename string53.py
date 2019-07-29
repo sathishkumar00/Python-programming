f3205837dc5d863211c56c06f7700c4e08205229
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-07-29 18:49:46 +0530

    Create string53.py

---
+++ string53.py
@@ -0,0 +1,42 @@
+d=input()
+e=input()
+h=[]
+k=[]
+if(d.isdigit()):
+       f=list(d)
+       g=list(e)
+else:
+       f=d.split(" ")
+       g=e.split(" ")
+if(len(d)==len(e)):
+       for i in range(len(f)-1):
+             if i not in g:
+                   h.append("2:")
+                   h.append(f[i])
+       for i in range(len(g)-1):
+             if i not in f:
+                   k.append("1:")
+                   k.append(g[i])
+       print(*k[0:2],sep="")
+       print(*h[0:2],sep="")
+else:
+   for i in f:
+       if i not in g:
+             h.append("2:")
+             h.append(i)
+   for i in g:
+       if i not in f:
+             k.append("1:")
+             k.append(i)
+   print(*k,sep=" ")
+   print(*h,sep=" ")
+if(len(f)==1 and len(g)==1):
+       for i in f:
+            h.append("2:")
+            h.append(i)
+       for i in g:
+            k.append("1:")
+            k.append(i)
+       print(*k,sep=" ")
+       print(*h,sep=" ")
+                   
