d03e6a988a68f44d07ee6da92902a9ed8f901647
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-08-08 09:57:49 +0530

    Update string58.py

--- string58.py
+++ string58.py
@@ -1,7 +1,9 @@
 c=input()
-if(c=="R P" or c=="P P" or c=="R R" or c=="S S"):
+if(c=="R P"):
       print("P")
 elif(c=="S P"):
       print("S")
 elif(c=="R S"):
       print("R")
+elif(c=="P P" or c=="R R" or c=="S S"):
+      print("D")
