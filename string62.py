8d374e3671dbcdb44e5259b8c4cd7a1f081ba17c
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-08-09 18:09:05 +0530

    Delete string62.py

--- string62.py
+++
@@ -1,7 +0,0 @@
-c=input().split(" ")
-for i in range(0,len(c)):
-      if(c[i]=="and" and len(c)>1):
-          c[i-1],c[i+1]=c[i+1],c[i-1]
-      elif(len(c)==1):
-          break
-print(*c,sep=" ")
