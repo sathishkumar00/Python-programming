2db2eb0a75a93faee1995eb1ba2a680bdd242e67
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-08-10 18:02:11 +0530

    Update string66.py

--- string66.py
+++ string66.py
@@ -2,12 +2,15 @@ c=list(input())
 d=['a','e','i','o','u']
 e=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 f=0
-for i in range(len(c)-1):
-           if(c[i] in e and c[i+1] in d):
-                f+=1
-           elif(c[i] in d and c[i+1] in e):
-                f+=1
-if((c[-1] in e and c[-2] in d) or (c[-1] in d and c[-2] in e)):
-      f+=1
-print(f)
+if(len(c)>1):
+        for i in range(len(c)-1):
+               if(c[i] in e and c[i+1] in d):
+                         f+=1
+               elif(c[i] in d and c[i+1] in e):
+                         f+=1
+        if((c[-1] in e and c[-2] in d) or (c[-1] in d and c[-2] in e)):
+                       f+=1
+        print(f)
+else:
+   print(f)
             
