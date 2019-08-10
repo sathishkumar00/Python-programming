1cdca82ccd926b51e01033c02a4b1e0b2baed541
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-08-10 17:55:20 +0530

    Update string66.py

--- string66.py
+++ string66.py
@@ -5,7 +5,7 @@ f=0
 for i in range(len(c)-1):
            if(c[i] in e and c[i+1] in d):
                 f+=1
-           elif(c[i] in d and c[i] in e):
+           elif(c[i] in d and c[i+1] in e):
                 f+=1
 if((c[-1] in e and c[-2] in d) or (c[-1] in d and c[-2] in e)):
       f+=1
