51ea88331f48eb66f90873996bfed5a1246c0d57
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-08-12 19:33:05 +0530

    Update number200.py

--- number200.py
+++ number200.py
@@ -2,8 +2,15 @@ from fractions import gcd
 c,d=list(map(int,input().split(" ")))
 e=1
 f=1
-for i in range(1,c+1):
-     e=e*i
-for i in range(1,d+1):
-     f=f*i
-print(gcd(e,f))
+c=str(c)
+if(len(c)>=7):
+     for i in range(1,d+1):
+          f=f*i
+     print(f)
+else:
+   c=int(c)
+   for i in range(1,c+1):
+          e=e*i
+   for i in range(1,d+1):
+          f=f*i
+   print(gcd(e,f))
