3c61aa6a3211406a0e8a27b4c09dc1d2851b0392
Prithamprince <51200147+Prithamprince@users.noreply.github.com>
2019-08-14 11:46:29 +0530

    Update number201.py

--- number201.py
+++ number201.py
@@ -1,6 +1,6 @@
 c=int(input())
-d=list(map(int,input().split(" "))
-e=list(map(int,input().split(" "))
+d=list(map(int,input().split(" ")))
+e=list(map(int,input().split(" ")))
 f={}
 for i in range(0,len(d)):
       f[e[i]]=d[i]
